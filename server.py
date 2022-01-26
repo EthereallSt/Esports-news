c4313db8b49521472d4ef3120e5f3b3526f28ea7
Stepan <stpokorn@ya.ru>
2022-01-26 17:49:12 +0300

    добавил парсер новостей python

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_news  
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    title = "Прогноз погоды"
+    title = "Новости Python"
     weather = weather_by_city('Moscow,Russia')
-    return render_template('index.html', page_title=title, weather=weather)
+    news_list = get_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 
 if __name__ == "__main__":
