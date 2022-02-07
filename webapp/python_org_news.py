7ad0db8da07b90b866c0dcad174576710f1fc63f
Stepan <stpokorn@ya.ru>
2022-02-07 18:26:17 +0300

    Исправил проблему с БД, Работает

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,13 +1,11 @@
 from datetime import datetime
- 
+
 import requests
 from bs4 import BeautifulSoup
 
 from webapp.model import db, News
 
-
 def get_html(url):
-    
     try:
         result = requests.get(url)
         result.raise_for_status()
@@ -31,8 +29,14 @@ def get_python_news():
             except ValueError:
                 published = datetime.now()
             save_news(title, url, published)
-             
+
 def save_news(title, url, published):
-    news_news = News(title=title, url=url, published=published)
-    db.session.add(news_news)
-    db.session.commit()
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
+
+
+            
